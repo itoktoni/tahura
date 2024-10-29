4f42f07b6ec904d950da02b5034dd0813916f5ad
itoktoni <itok.toni@gmail.com>
2024-10-29 12:01:04 +0700

    update

---
+++ app/Console/Commands/TestCommand.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Dao\Models\Core\User;
+use App\Mail\CreateScheduleReceiveRunningTools;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test:command';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'untuk mengirimkan whatsapp otomatis';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+            $this->info('Success');
+    }
+}
